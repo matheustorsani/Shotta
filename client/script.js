b9c3ab4bf19df3e085b2359dc4f41ea13e5f4e3b
tavinzn <otaviosantosandrigo08@gmail.com>
2025-07-07 13:35:22 -0300

    json finish

--- client/script.js
+++ client/script.js
@@ -1,8 +1,26 @@
-const nome = document.getElementById("#username")
-const message = document.querySelectorById("#message")
+
+
 const msg = document.querySelector(".text")
 const nomeMsg = document.querySelector(".username-text")
 
-function enviarmsg(){
+function criarMsg(){
+    const nome = document.getElementById("username").value;
+    const msgEsc = document.getElementById("message").value;
 
+    if (!nome || !msgEsc) return;
+
+    const mensagem = {
+        nome: nome,
+        message: msgEsc
+    };
+
+    const template = document.getElementById("message-template");
+    const novaMsg = template.cloneNode(true);
+    novaMsg.style.display = "block";
+    
+    novaMsg.querySelector(".username-text").textContent = mensagem.nome;
+    novaMsg.querySelector(".text").textContent = mensagem.message;
+
+    document.querySelector(".messages").appendChild(novaMsg);
+    document.getElementById("message").value = "";
 }
