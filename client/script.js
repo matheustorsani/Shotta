d996afb0373af4155c7cf9f054678663e1776b44
tavinzn <otaviosantosandrigo08@gmail.com>
2025-07-07 00:42:58 -0300

    falta só o json

---
+++ client/script.js
@@ -0,0 +1,8 @@
+const nome = document.getElementById("#username")
+const message = document.querySelectorById("#message")
+const msg = document.querySelector(".text")
+const nomeMsg = document.querySelector(".username-text")
+
+function enviarmsg(){
+
+}
